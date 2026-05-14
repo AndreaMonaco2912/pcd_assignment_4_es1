52fce999dadee12b5bf6daa4dbfcc26dda486b7f
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-05-14 12:15:38 +0200

    Solved problem with zones

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,7 @@
 package assignment3
 
+import SmartAlarmSystem.Command
 
-import assignment3.SmartAlarmSystem.Command.{EnterPin, EnterTimer, SensorDetection}
 import org.apache.pekko.actor.typed.*
 import org.apache.pekko.actor.typed.scaladsl.*
 
@@ -10,33 +10,33 @@ import scala.concurrent.duration.DurationInt
 @main
 def main(): Unit =
   val system = ActorSystem(SmartAlarmSystem(), "SmartAlarmSystem")
-  system ! SensorDetection("Hall")
+  system ! Command.SensorDetection("Hall")
   Thread.sleep(1000)
 
-  system ! EnterPin("222")
+  system ! Command.EnterPin("222")
   Thread.sleep(1000)
 
-  system ! EnterPin("1234")// exit house arming (timer started)
-  system ! EnterPin("1234")//disarmed immediately
-  system ! EnterPin("1234")//rearmed
+  system ! Command.EnterPin("1234")// exit house arming (timer started)
+  system ! Command.EnterPin("1234")//disarmed immediately
+  system ! Command.EnterPin("1234")//rearmed
   Thread.sleep(1200)
 
-  system ! SensorDetection("Hall")
+  system ! Command.SensorDetection("Hall")
 
-  system ! EnterPin("222")// typo in pin
-  system ! EnterPin("1234")// correct pin
+  system ! Command.EnterPin("222")// typo in pin
+  system ! Command.EnterPin("1234")// correct pin
 
   Thread.sleep(500)
 
-  system ! EnterPin("1234") //exit again
+  system ! Command.EnterPin("1234") //exit again
   Thread.sleep(1500) //system armed
 
-  system ! SensorDetection("Hall")
+  system ! Command.SensorDetection("Hall")
   Thread.sleep(2000)
-  system ! EnterPin("222") //should not nino
+  system ! Command.EnterPin("222") //should not nino
   Thread.sleep(2000) // should nino
-  system ! EnterPin("222")
-  system ! EnterPin("1234")
+  system ! Command.EnterPin("222")
+  system ! Command.EnterPin("1234")
 
 object SmartAlarmSystem:
   enum Command:
@@ -52,6 +52,7 @@ object SmartAlarmSystem:
   val exitDuration = 1.seconds
   val enterDuration = 3.seconds
 
+  val allZones = Set("Hall", "Kitchen", "BedRoom")
   var armedZones = Set.empty[String]
 
   def apply(): Behavior[Command] = Behaviors.setup: ctx =>
@@ -60,7 +61,7 @@ object SmartAlarmSystem:
   def disarmed(ctx: ActorContext[Command]): Behavior[Command] = Behaviors.receiveMessagePartial:
     case Command.EnterPin(pin) if pin == correctPin =>
       ctx.log.info("Correct pin, fully armed House! You have: " + exitDuration + " to leave your home")
-      armedZones = armedZones.empty
+      armedZones = allZones
       Behaviors.withTimers: timers =>
         timers.startSingleTimer(ExitTimer, exitDuration)
         exitTimer(ctx)
@@ -96,7 +97,7 @@ object SmartAlarmSystem:
     case Command.EnterPin(_) =>
       ctx.log.info("Wrong Pin Fast to Alarm!!")
       alarm(ctx)
-    case Command.SensorDetection(_) =>
+    case Command.SensorDetection(zone) if armedZones.contains(zone) =>
       ctx.log.info("Movement detected!!!")
       Behaviors.withTimers: timers =>
         timers.cancelAll()
